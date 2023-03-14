1c16d4eb54ae90bf20f507c7d3cebf3abaac7598
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-03-14 10:39:45 -0400

    Define types and a component for a Text component that can render different element types with different colors

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,30 +1,49 @@
 import { PropsWithChildren, ElementType, ComponentPropsWithoutRef, forwardRef, ComponentPropsWithRef } from "react";
 
+// Define a type that will be used to create a rainbow of colors
 type Rainbow = "red" | "orange" | "yellow" | "green" | "blue" | "indigo" | "violet";
 
+// Define a type that will be used to create a prop
+// that will be used to define the type of element
+// to be rendered to the DOM
 type AsProp<C extends ElementType> = {
   as?: C;
 };
 
+// Define a type that will be used to create
+// the props that will be passed to the component
 type TextProps = {
   color?: Rainbow | string;
 };
 
+// Define a type that will be used to create a type that
+// will be used to omit the props from the component
+// that are used to create the type of element to be rendered
 type PropsToOmit<C extends ElementType, P> = keyof (AsProp<C> & P);
 
+// Define a type that will be used to create the props
+// that will be passed to the component
 type PolymorphicComponent<C extends ElementType, Props = {}> = PropsWithChildren<Props & AsProp<C>> &
   Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
 
+// Define a type that will be used to create the props
+// that will be passed to the component
 type Props<C extends ElementType, P> = PolymorphicComponent<C, P>;
 
+// Define a type that will be used to create the props
+// that will be passed to the component
 type PolimosphicComponentPropsWithRef<C extends ElementType, P> = PolymorphicComponent<C, P> & {
   ref?: PolimosphicRef<C>;
 };
 
+// Define a type that will be used to create the props
+// that will be passed to the component
 type PolimosphicRef<C extends ElementType> = ComponentPropsWithRef<C>["ref"];
 
+// Define a type that will be used to create the component
 type TextComponent = <C extends ElementType>(props: PolimosphicComponentPropsWithRef<C, TextProps>) => React.ReactElement | null;
 
+// Define the component
 export const Text: TextComponent = forwardRef(function Text<C extends ElementType = "span">(
   { as, color, style, children, ...rest }: Props<C, TextProps>,
   ref: PolimosphicRef<C>
